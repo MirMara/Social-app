4854a953a1d8ba953edfea04f2e4a83b7588f1b8
Miriana Maranzano <mirianamaranzano2@gmail.com>
2022-01-13 12:39:05 +0100

    testing-header

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,11 +1,55 @@
-// import { render } from '@testing-library/react';
-// import Header from './Header';
+import { render, screen } from '@testing-library/react';
+import Header from './Header.js';
+import { createMemoryHistory } from "history";
+import React from "react";
+import { Router } from "react-router-dom";
 
-// test("checks markup in <Header /> component", () => {
-//     const {container} = render(<Header />);
 
-//     const header = container.querySelector("header");
+test('<Header/> component as default', () => {
+    const history = createMemoryHistory();
+    render(
+        <Router location={history.location} navigator={history}>
+            <Header />
+        </Router>);
 
-//     expect(header).toBeTruthy();
 
-// });
+    const msg = screen.getByText('App');
+    expect(msg).toBeInTheDocument();
+});
+
+test('<Header/> component with custom name', () => {
+    const history = createMemoryHistory();
+    render(
+        <Router location={history.location} navigator={history}>
+            <Header name={"Feisbrut"} />
+        </Router>);
+
+
+
+    const title = screen.getByText('Feisbrut');
+
+    expect(title).toBeInTheDocument();
+
+});
+
+test(' <Header/> component with custom with custom links', () => {
+    const history = createMemoryHistory();
+
+    const links = [
+        { link: "/", label: "Home" },
+        { link: "/messages", label: "Messages" },
+        { link: "/friends", label: "Friends" },
+        { link: "/login", label: "Login" },
+    ];
+
+    const { container } = render(
+        <Router location={history.location} navigator={history}>
+            <Header links={links} />
+        </Router>);
+
+
+
+    const lis = container.querySelectorAll('nav li');
+    expect(lis.length).toBe(lis.length);
+
+});
